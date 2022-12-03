4ceb59923b7987ecf514a3728c0a6e3adf0f37bd
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2022-12-03 20:58:51 +0100

    Refactored calendars

---
+++ 2015/calendar.py
@@ -0,0 +1,9 @@
+from advent_calendar import AdventCalendar
+
+advent_calendar = AdventCalendar(2015)
+
+advent_calendar.run_days()
+advent_calendar.import_days()
+advent_calendar.print_days()
+
+    
---
+++ general/advent_calendar.py
@@ -0,0 +1,29 @@
+import os
+import re
+import sys
+sys.path.append("general/")
+
+from day import Day, create_days_from_json
+
+class AdventCalendar:
+    def __init__(self, year: int|str) -> None:
+        self.year = int(year)
+        self.days = []
+        
+        self.PATH = f"{os.path.dirname(__file__)}/../{year}"
+    
+    def run_days(self) -> None:
+        days_path = f"{self.PATH}/days/"
+        day_files:list[str] = re.findall(r'day\d+\.py', str(os.listdir(days_path)))
+
+        for day_file in day_files:
+            os.system(f"python {days_path}{day_file} -n")
+        
+    def import_days(self):
+        self.days = create_days_from_json(f"{self.PATH}/output/out.json")
+    
+    def print_days(self):
+        for day in self.days:
+            print(day)
+    
+    
