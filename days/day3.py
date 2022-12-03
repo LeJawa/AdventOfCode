526cd09dec6515a90628433112d6c89a3c112cd4
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2022-12-03 09:15:13 +0100

    started day3

--- days/day3.py
+++ days/day3.py
@@ -1,14 +1,36 @@
 from day import Day
 
-DAY_NUMBER = 0
+DAY_NUMBER = 3
 
 def get_input_lines() -> list[str]:
     with open(f"inputs/day{DAY_NUMBER}.txt", 'r') as f:
         lines = f.readlines()
+    
     return lines
 
+def get_item_priority(item: str) -> int:
+    is_lower = item == item.lower()
+    
+    if is_lower:
+        return ord(item) - 96
+    else:
+        return ord(item) - 65 + 27
+    
+
 def get_description_and_result() -> tuple[str, str]:
-    lines = get_input_lines()
+    rucksacks = get_input_lines()
+    
+    total_priority = 0
+    
+    for rucksack in rucksacks:
+        compartment_size = int(len(rucksack)/2)
+
+        first_compartment = rucksack[:compartment_size]
+        second_compartment = rucksack[compartment_size:-1]
+        
+        print(first_compartment)
+        print(second_compartment)
+        
     
     pass
     
